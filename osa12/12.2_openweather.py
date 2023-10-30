83b0c81dfe36d5a60051cfb8c012711f5bb4046b
EinoRuuth <eino@ruuth.net>
2023-10-30 13:56:15 +0200

    pikku muutoksia

--- osa12/12.2_openweather.py
+++ osa12/12.2_openweather.py
@@ -6,12 +6,18 @@ paikkakunta = input("Anna paikkakunta: ")
 
 load_dotenv()
 apikey = os.getenv('apikey')
-lokaatiokysely = f"http://api.openweathermap.org/geo/1.0/direct?q={paikkakunta}&limit=1&appid={apikey}"
+try:
+    lokaatiokysely = f"http://api.openweathermap.org/geo/1.0/direct?q={paikkakunta}&limit=1&appid={apikey}"
+except requests.exceptions.RequestException as e:
+    print ("Hakua ei voitu suorittaa.")
 lokaatio = requests.get(lokaatiokysely).json()
 if len(lokaatio) == 0:
     exit("lokaatiota ei löytnyt")
 koordinaatit = (lokaatio[0]["lon"], lokaatio[0]["lat"])
-sääkysely = f"https://api.openweathermap.org/data/2.5/weather?lat={koordinaatit[1]}&lon={koordinaatit[0]}&appid={apikey}&units=metric"
+try:
+    sääkysely = f"https://api.openweathermap.org/data/2.5/weather?lat={koordinaatit[1]}&lon={koordinaatit[0]}&appid={apikey}&units=metric"
+except requests.exceptions.RequestException as e:
+    print ("Hakua ei voitu suorittaa.")
 sää = requests.get(sääkysely).json()
 temperature = sää["main"]["temp"]
 säätila = sää["weather"][0]["main"]
